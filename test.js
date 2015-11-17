42439b7aaf56272caa26ce8aa67927f2d4140142
Sam Verschueren <sam.verschueren@gmail.com>
2015-11-17 20:57:20 +0100

    Use destructuring assignment

--- test.js
+++ test.js
@@ -6,14 +6,14 @@ const cwd = path.join(__dirname, 'fixture');
 const pkgPath = path.join(__dirname, 'package.json');
 
 test('async', async t => {
-	const x = await fn({cwd: cwd});
+	const x = await fn({cwd});
 
 	t.is(x.pkg.name, 'read-pkg-up');
 	t.is(x.path, pkgPath);
 });
 
 test('sync', t => {
-	const x = fn.sync({cwd: cwd});
+	const x = fn.sync({cwd});
 
 	t.is(x.pkg.name, 'read-pkg-up');
 	t.is(x.path, pkgPath);
