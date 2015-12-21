d8672a2c23fa3142caf23930fae6c677423e2bf7
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-21 16:51:14 +0100

    remove now moot XO ignore

--- test.js
+++ test.js
@@ -7,15 +7,12 @@ const pkgPath = path.resolve('.', 'package.json');
 
 test('async', async t => {
 	const x = await fn({cwd});
-
 	t.is(x.pkg.name, 'read-pkg-up');
 	t.is(x.path, pkgPath);
 });
 
 test('sync', t => {
 	const x = fn.sync({cwd});
-
 	t.is(x.pkg.name, 'read-pkg-up');
 	t.is(x.path, pkgPath);
-	t.end();
 });
