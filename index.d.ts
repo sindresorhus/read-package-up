8ac3abd648c78c52e6ccaf487ea4fc4a0c20e554
Louis Orleans <louis@orleans.io>
2017-09-19 10:59:33 -0700

    ✨ Adding TypeScript typings

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+declare module 'read-pkg-up' {
+  interface IReadPkgUp {
+    (options?: IOptions): Promise<IPackage>;
+
+    sync (options?: IOptions): IPackage;
+  }
+
+  interface IOptions {
+    /**
+     * Directory to start looking for a package.json file.
+     *
+     * @default '.'
+     */
+    cwd?: string;
+    /**
+     * [Normalize](https://github.com/npm/normalize-package-data#what-normalization-currently-entails) the package data.
+     *
+     * @default true
+     */
+    normalize?: boolean;
+  }
+
+  interface IPackage {
+    [key: string]: any;
+  }
+
+  const read_pkg_up: IReadPkgUp;
+
+  export = read_pkg_up;
+}
