ede847d421b9e740a19618d3fabe5272b5cddd9c
Louis Orleans <louis@orleans.io>
2018-10-19 19:18:51 -0700

    ✅ add test for missing package

--- test.js
+++ test.js
@@ -9,10 +9,18 @@ test('async', async t => {
 	const x = await m({cwd});
 	t.is(x.pkg.name, 'read-pkg-up');
 	t.is(x.path, pkgPath);
+
+	const y = await m({cwd: '/'});
+	t.is(y.pkg, undefined);
+	t.is(y.path, undefined);
 });
 
 test('sync', t => {
 	const x = m.sync({cwd});
 	t.is(x.pkg.name, 'read-pkg-up');
 	t.is(x.path, pkgPath);
+
+	const y = m.sync({cwd: '/'});
+	t.is(y.pkg, undefined);
+	t.is(y.path, undefined);
 });
