b4fbcf726f1bc23d963c8eb0ce5ad334d3ad8a93
Louis Orleans <louis@orleans.io>
2018-10-19 18:30:49 -0700

    🐛 fix return type

--- index.d.ts
+++ index.d.ts
@@ -4,12 +4,17 @@ export {PackageMetadata} from 'read-pkg';
 
 export interface Options extends FindUpOptions, ReadPkgOptions {}
 
+export interface PackageInfo {
+	pkg: PackageMetadata;
+	path: string;
+}
+
 /** Returns a `Promise` for the result object. */
-declare function readPkgUp(options?: Options): Promise<PackageMetadata>;
+declare function readPkgUp(options?: Options): Promise<PackageInfo>;
 
 declare namespace readPkgUp {
 	/** Returns the result object. */
-	function sync(options?: Options): PackageMetadata;
+	function sync(options?: Options): PackageInfo;
 }
 
 export default readPkgUp;
